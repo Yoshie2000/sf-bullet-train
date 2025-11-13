b03d3f2e45d2423b0aabaf4e35699076b38b3bd6
Yoshie2000 <patrick.leonhardt@gmx.net>
2025-11-13 21:34:15 +0100

    Update bullet, working checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-use crate::outputs::OutputBuckets;
 use bullet::{
-    NetworkTrainer, default::inputs::{Chess768, Factorised, SparseInputType}, nn::{
-        Activation, ExecutionContext, Graph, InitSettings, NetworkBuilder, Node, Shape, optimiser::{RangerOptimiser, RangerParams}
+    game::{formats::sfbinpack::{TrainingDataEntry, chess::{r#move::MoveType, piecetype::PieceType}}, inputs::{self, Chess768, Factorised, Factorises, SparseInputType}, outputs::{self, OutputBuckets}}, nn::{
+        InitSettings, Shape, optimiser::{Ranger}
     }, trainer::{
-        default::{
-            Trainer, formats::sfbinpack::{
-                TrainingDataEntry, chess::{r#move::MoveType, piecetype::PieceType}
-            }, inputs, loader, outputs
-        },
-        save::{Layout, QuantTarget, SavedFormat},
+        save::SavedFormat,
         schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
         settings::LocalSettings,
-    }
+    }, value::{ValueTrainerBuilder, loader}
 };
 use bulletformat::ChessBoard;
 
@@ -46,7 +40,7 @@ impl SparseInputType for SfInputs {
     type RequiredDataType = ChessBoard;
 
     fn description(&self) -> String {
-        ""
+        "".to_string()
     }
 
     fn is_factorised(&self) -> bool {
@@ -58,17 +52,16 @@ impl SparseInputType for SfInputs {
         let make_index = |perspective, sq, pc, ksq| -> usize {
             let flip = 56 * perspective;
             let orientation = if ksq % 8 > 3 { 0 } else { 7 };
-            let color = usize::from(piece & 8 > 0);
+            let color = usize::from(pc & 8 > 0);
             let pctype = usize::from(pc & 7);
-            let sf_pc = 2 * pctype + (1 - color).max(if pctype == 5 { 0 } else { 1 });
-            assert!(pctype <= 5);
-            assert!(sf_pc <= 11);
+            let sf_pc = 2 * pctype + (1 - color);
+            let sf_pc = sf_pc.min(10); // no king feature on index 11
             return (sq ^ flip ^ orientation) + 64 * sf_pc + 64 * 11 * SfInputs::BUCKETS[ksq ^ flip];
         };
 
         for (piece, square) in pos.into_iter() {
-            let stm = make_index(0, square, piece, pos.our_ksq());
-            let ntm = make_index(1, square, piece, pos.opp_ksq());
+            let stm = make_index(0 as usize, square as usize, piece, pos.our_ksq() as usize);
+            let ntm = make_index(1 as usize, square as usize, piece, pos.opp_ksq() as usize);
             f(stm, ntm);
         }
     }
@@ -77,18 +70,33 @@ impl SparseInputType for SfInputs {
         32
     }
 
-    fn merge_factoriser(&self, unmerged: Vec<f32>) -> Vec<f32> {
-        
-    }
-
     fn num_inputs(&self) -> usize {
         704 * 32
     }
 
     fn shorthand(&self) -> String {
-        ""
+        "".to_string()
     }
 
+}
+
+impl Factorises<SfInputs> for Chess768 {
+    fn derive_feature(&self, _: &SfInputs, feat: usize) -> Option<usize> {
+        let mut feature = feat % 704;
+
+        let square = feat % 64;
+        let bucket = feat / 704;
+        let piece = feature / 64;
+        if piece == 10 && (square % 8 <= 3 || SfInputs::BUCKETS[square] != bucket) {
+            // If the feature corresponds to a king,
+            // and the king is in a different bucket or in an impossible
+            // position due to mirroring, it's the ntm king,
+            // which is encoded differently in Chess768 inputs
+            feature += 64; // effectively makes "piece" become 11
+        }
+
+        Some(feature)
+    }
 }
 
 type InputFeatures = Factorised<SfInputs, Chess768>;
@@ -97,64 +105,76 @@ const L2: usize = 15;
 const L3: usize = 32;
 
 fn main() {
-    let inputs = InputFeatures::default();
+    let inputs = InputFeatures::from_parts(SfInputs::default(), Chess768::default());
 
     let output_buckets = SfMaterialCount::default();
     let num_inputs = <InputFeatures as inputs::SparseInputType>::num_inputs(&inputs);
-    let max_active = <InputFeatures as inputs::SparseInputType>::max_active(&inputs);
-    let num_buckets = <SfMaterialCount as outputs::OutputBuckets<_>>::BUCKETS;
-
-    let (graph, output_node) = build_network(num_inputs, max_active, num_buckets);
+    const NUM_OUTPUT_BUCKETS: usize = <SfMaterialCount as outputs::OutputBuckets<_>>::BUCKETS;
 
     let saved_format = vec![
-        SavedFormat::new("l0b", QuantTarget::I16(255), Layout::Normal),
-        SavedFormat::new("l0w", QuantTarget::I16(255), Layout::Normal),
-        SavedFormat::new("pst", QuantTarget::I32(255), Layout::Normal),
-        SavedFormat::new("l1b", QuantTarget::I32(64 * 255), Layout::Normal).add_transform(
-            |graph, mut weights| {
-                let fact = graph.get_weights("l1_factb").get_dense_vals().unwrap();
-                add_factoriser(&mut weights, &fact, L2 + 1);
-                weights
-            },
-        ),
-        SavedFormat::new(
-            "l1w",
-            QuantTarget::I8(64),
-            Layout::Transposed(Shape::new(NUM_BUCKETS * (L2 + 1), L1)),
-        )
-        .add_transform(|graph, mut weights| {
-            let fact = graph.get_weights("l1_factw").get_dense_vals().unwrap();
-            let fact = SavedFormat::transpose(Shape::new(L2 + 1, L1), &fact);
-            add_factoriser(&mut weights, &fact, (L2 + 1) * L1);
-            weights
+        SavedFormat::id("l0b").round().quantise::<i16>(255),
+        SavedFormat::id("l0w").round().quantise::<i16>(255),
+        SavedFormat::id("pst").round().quantise::<i32>(255),
+        SavedFormat::id("l1b").round().quantise::<i32>(64 * 255).transform(|store, weights| {
+            let fact = store.get("l1_factb").values.repeat(NUM_OUTPUT_BUCKETS);
+            weights.into_iter().zip(fact).map(|(a, b)| a + b).collect()
         }),
-        SavedFormat::new("l2b", QuantTarget::I32(64 * 127), Layout::Normal),
-        SavedFormat::new(
-            "l2w",
-            QuantTarget::I8(64),
-            Layout::Transposed(Shape::new(NUM_BUCKETS * L3, L2 * 2)),
-        ),
-        SavedFormat::new("l3b", QuantTarget::I32(16 * 600), Layout::Normal),
-        SavedFormat::new(
-            "l3w",
-            QuantTarget::I8(600 * 16 / 127),
-            Layout::Transposed(Shape::new(NUM_BUCKETS, L3)),
-        ),
+        SavedFormat::id("l1w").round().quantise::<i8>(64).transpose().transform(|store, weights| {
+            let fact = store.get("l1_factw").values.repeat(NUM_OUTPUT_BUCKETS);
+            weights.into_iter().zip(fact).map(|(a, b)| a + b).collect()
+        }),
+        SavedFormat::id("l2b").round().quantise::<i32>(64 * 127),
+        SavedFormat::id("l2w").round().quantise::<i8>(64).transpose(),
+        SavedFormat::id("l3b").round().quantise::<i32>(16 * 600),
+        SavedFormat::id("l3w").round().quantise::<i8>(600 * 16 / 127).transpose(),
     ];
 
-    let mut trainer = Trainer::<RangerOptimiser, _, _>::new(
-        graph,
-        output_node,
-        RangerParams::default(),
-        inputs,
-        output_buckets,
-        saved_format,
-        false,
-    );
+    let mut trainer = ValueTrainerBuilder::default()
+        .dual_perspective()
+        .optimiser(Ranger)
+        .loss_fn(|output, targets| output.sigmoid().power_error(targets, 2.6))
+        .inputs(inputs)
+        .output_buckets(output_buckets)
+        .save_format(saved_format.as_slice())
+        .build(|builder, stm, ntm, buckets| {
+            // trainable weights
+            let l0 = builder.new_affine("l0", num_inputs, L1);
+            let l1 = builder.new_affine("l1", L1, NUM_OUTPUT_BUCKETS * (L2 + 1));
+            let l1_fact = builder.new_affine("l1_fact", L1, L2 + 1);
+            let l2 = builder.new_affine("l2", L2 * 2, NUM_OUTPUT_BUCKETS * L3);
+            let l3 = builder.new_affine("l3", L3, NUM_OUTPUT_BUCKETS);
+            let pst = builder.new_weights(
+                "pst",
+                Shape::new(NUM_OUTPUT_BUCKETS, num_inputs),
+                InitSettings::Zeroed,
+            );
 
-    trainer.mark_weights_as_input_factorised(&["l0w", "pst"]);
+            // inference
+            let stm_subnet = l0.forward(stm).crelu();
+            let ntm_subnet = l0.forward(ntm).crelu();
+            let mut out = stm_subnet.concat(ntm_subnet);
 
-    println!("Params: {}", trainer.optimiser().graph.get_num_params());
+            out = out.pairwise_mul();
+            out = l1.forward(out).select(buckets) + l1_fact.forward(out);
+
+            let skip_neuron = out.slice_rows(15, 16);
+            out = out.slice_rows(0, 15);
+
+            out = out.concat(out.abs_pow(2.0));
+            out = out.crelu();
+
+            out = l2.forward(out).select(buckets).screlu();
+            out = l3.forward(out).select(buckets);
+
+            let stm_pst = pst.matmul(stm).select(buckets);
+            let ntm_pst = pst.matmul(ntm).select(buckets);
+            let pst_out = stm_pst.linear_comb(0.5, ntm_pst, -0.5);
+            out = out + skip_neuron + pst_out;
+
+            out
+        });
+
+    println!("Params: {}", trainer.optimiser.graph.get_num_params());
 
     let schedule = TrainingSchedule {
         net_id: "test".to_string(),
@@ -182,7 +202,7 @@ fn main() {
     };
 
     let data_loader = {
-        let file_path = "/mnt/g/stockfish-data/leela96-filt-v2.min.binpack";
+        let file_path = "/mnt/d/Chess Data/aprilmay2022/T79-apr2022-12tb7p.binpack";
         let buffer_size_mb = 1024;
         let threads = 8;
         fn filter(entry: &TrainingDataEntry) -> bool {
@@ -190,7 +210,7 @@ fn main() {
                 && !entry.pos.is_checked(entry.pos.side_to_move())
                 && entry.score.unsigned_abs() <= 10000
                 && entry.mv.mtype() == MoveType::Normal
-                && entry.pos.piece_at(entry.mv.to).piece_type() == PieceType::None
+                && entry.pos.piece_at(entry.mv.to()).piece_type() == PieceType::None
         }
 
         loader::SfBinpackLoader::new(file_path, buffer_size_mb, threads, filter)
@@ -201,63 +221,4 @@ fn main() {
     let eval =
         400.0 * trainer.eval("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 | 0 | 0.0");
     println!("Eval: {eval:.3}cp");
-}
-
-fn build_network(num_inputs: usize, max_active: usize, num_buckets: usize) -> (Graph, Node) {
-    let builder = NetworkBuilder::default();
-
-    // inputs
-    let stm = builder.new_sparse_input("stm", Shape::new(num_inputs, 1), max_active);
-    let nstm = builder.new_sparse_input("nstm", Shape::new(num_inputs, 1), max_active);
-    let targets = builder.new_dense_input("targets", Shape::new(1, 1));
-    let buckets = builder.new_sparse_input("buckets", Shape::new(num_buckets, 1), 1);
-
-    // trainable weights
-    let l0 = builder.new_affine("l0", num_inputs, L1);
-    let l1 = builder.new_affine("l1", L1, num_buckets * (L2 + 1));
-    let l1_fact = builder.new_affine("l1_fact", L1, L2 + 1);
-    let l2 = builder.new_affine("l2", L2 * 2, num_buckets * L3);
-    let l3 = builder.new_affine("l3", L3, num_buckets);
-    let pst = builder.new_weights(
-        "pst",
-        Shape::new(num_buckets, num_inputs),
-        InitSettings::Zeroed,
-    );
-
-    // inference
-    let stm_subnet = l0.forward(stm).activate(Activation::CReLU);
-    let ntm_subnet = l0.forward(nstm).activate(Activation::CReLU);
-    let mut out = stm_subnet.concat(ntm_subnet);
-
-    out = out.pairwise_mul_post_affine_dual();
-    out = l1.forward(out).select(buckets) + l1_fact.forward(out);
-
-    let skip_neuron = out.slice_rows(15, 16);
-    out = out.slice_rows(0, 15);
-
-    out = out.concat(out.activate(Activation::Square));
-    out = out.activate(Activation::CReLU);
-
-    out = l2.forward(out).select(buckets).activate(Activation::SCReLU);
-    out = l3.forward(out).select(buckets);
-
-    let stm_pst = pst.matmul(stm).select(buckets);
-    let ntm_pst = pst.matmul(nstm).select(buckets);
-    let pst_out = stm_pst.linear_comb(0.5, ntm_pst, -0.5);
-    out = out + skip_neuron + pst_out;
-
-    let pred = out.activate(Activation::Sigmoid);
-    pred.mpe(targets, 2.6);
-
-    // graph, output node
-    let output_node = out.node();
-    (builder.build(ExecutionContext::default()), output_node)
-}
-
-const NUM_BUCKETS: usize = <SfMaterialCount as outputs::OutputBuckets<_>>::BUCKETS;
-
-fn add_factoriser(weights: &mut [f32], factoriser: &[f32], size: usize) {
-    for i in 0..NUM_BUCKETS * size {
-        weights[i] += factoriser[i % size]
-    }
 }
